027bdc36497082e5d0759521530091da656a1a7e
walle <michelle210181@gmail.com>
2020-01-04 21:05:50 +0800

    create landing page

---
+++ main.js
@@ -0,0 +1,35 @@
+const imgs = document.querySelectorAll('.img-wrapper>img')
+const reason = document.getElementById('reason')
+const ingredient = document.getElementById('ingredient')
+const offer = document.getElementById('offer')
+
+imgs.forEach(img => {
+  img.addEventListener('mouseenter', e => {
+    const parentDiv = e.target.parentNode
+    img.classList.add('visible')
+    parentDiv.classList.remove('transition', 'zoomOut')
+    parentDiv.classList.add('transition', 'animated', 'zoomIn')
+  })
+
+  img.addEventListener('mouseleave', e => {
+    const parentDiv = e.target.parentNode
+    parentDiv.classList.add('transition', 'zoomOut')
+    parentDiv.classList.remove('transition', 'zoomIn')
+  })
+})
+
+document.addEventListener('scroll', () => {
+  let height = window.scrollY
+
+  if (height > 250) {
+    reason.classList.replace('hidden', 'fadeInLeft')
+  }
+
+  if (height > 400) {
+    ingredient.classList.replace('hidden', 'fadeInRight')
+  }
+
+  if (height > 550) {
+    offer.classList.replace('hidden', 'fadeInLeft')
+  }
+})
